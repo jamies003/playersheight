c8854dcdc19a50f96490a580700710a976d6252c
Jamie Stewart <stwjme@gmail.com>
2020-04-08 22:24:17 -0400

    Persons Added

--- playersheight.js
+++ playersheight.js
@@ -7,3 +7,8 @@ function Person (name, age, school, league) {
         return ++this.age;
     };
 }
+let person1 = new Person ("Jimm", 19, "St.Marys", "D League");
+let person2 = new Person ("Carl", 17, "Jump Start", "D League");
+let person3 = new Person ("Peter", 18, "St.Marys", "D League");
+let person4 = new Person ("Carl", 16, "A Stars", "B League");
+
